db5913af900aadc0d5e47f78baf1b75445ed5246
minjaeKang <blueie2@naver.com>
2024-02-01 09:19:06 +0900

    g

--- app/src/main/java/minjaelab/sketchbook/MainActivity.java
+++ app/src/main/java/minjaelab/sketchbook/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // SketchBook - place for tests, Android - Java
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
